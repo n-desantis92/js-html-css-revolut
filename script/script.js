18c9c7ac165472c046878ff776c5433a46181da5
n-desantis92 <dsnwebdev@gmail.com>
2021-03-16 11:30:10 +0100

    aggiustato toggleClass

--- script/script.js
+++ script/script.js
@@ -1,11 +1,21 @@
+$(function functionName() {
 
 
-var botNav = $(".bot-menu");
-var listaBotNav = $(".menu-list");
+  var botNav = $("li.bot-menu");
 
-botNav.click(
-  function () {
-    $(this).children(listaBotNav).toggleClass("active");
-  }
+  $(".nav-right > li" ).mouseenter(
+    function () {
+      $(this).siblings("li").children("ul.menu-list").removeClass("active");
+      $(this).children("ul.menu-list").addClass("active");
+    }
+  )
 
-);
+  botNav.click(
+    function () {
+
+      $(this).children("ul.menu-list").toggleClass("active");
+    }
+
+  );
+
+})
